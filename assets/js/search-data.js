cec348025b9a32d3a4c6c3f2f315c911f5e6732e
Bendjelal <bendjelal.yanis@gmail.com>
2026-05-14 19:50:53 +0000

    Deploying to gh-pages from @ Bendjelal/bendjelal.fr@696220821c8b2cc5fc184cce1e45899f531d135c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -180,7 +180,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv_academique_complet.pdf", "_blank");
         },
       },{
         id: 'social-email',
